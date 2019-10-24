5904a5c61a8216c24eeef1cf39966f44be91230c
marusazerjal <marusa.zerjal@anu.edu.au>
2019-10-24 15:01:46 +1100

    plot masterflat

--- main.py
+++ main.py
@@ -142,8 +142,8 @@ def main():
     else:
         print('MASTERFLAT', masterflat)
         plt.imshow(masterflat, aspect="auto")
-        plt.show()
         order_masks = mask_orders.return_masks(masterflat, config=config)
+    plt.show()
 
     masterflat_extracted = extract_order.mask_order(masterflat,order_masks, binning=binning)
 
